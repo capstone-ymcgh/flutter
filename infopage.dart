2e8717ed35e8ea12469205f61cf35dfd8b3f5006
dlghdyd <hoyujiin@naver.com>
2024-05-20 00:38:39 +0900

    식단정보및수정페이지 5/20수정

--- infopage.dart
+++ infopage.dart
@@ -2,8 +2,20 @@ import 'package:flutter/material.dart';
 
 
 class InfoPage extends StatelessWidget {
+  final List<String> texts;
+
+  InfoPage(this.texts);
+
   @override
   Widget build(BuildContext context) {
+    String Text1 = texts.isNotEmpty ? texts.first : ""; // 첫 번째 텍스트 가져오기
+    String Text2 = texts.length > 1 ? texts[1] : ""; // 세 번째 텍스트 가져오기
+    String Text3 = texts.length > 2 ? texts[2] : ""; // 네 번째 텍스트 가져오기
+    String Text4 = texts.length > 3 ? texts[3] : ""; // 다섯 번째 텍스트 가져오기
+    String Text5 = texts.length > 4 ? texts[4] : ""; // 여섯 번째 텍스트 가져오기
+    String Text6 = texts.length > 5 ? texts[5] : ""; // 일곱 번째 텍스트 가져오기
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text('식단 정보 및 수정'),
@@ -15,7 +27,7 @@ class InfoPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             InfoText(
-              text: 'Text 1',
+              text: Text1,
               onTap: () {
                 // Text 1이 눌렸을 때 실행되는 동작
                 ScaffoldMessenger.of(context).showSnackBar(
@@ -25,7 +37,7 @@ class InfoPage extends StatelessWidget {
             ),
             SizedBox(height: 16.0),
             InfoText(
-              text: 'Text 2',
+              text: Text2,
               onTap: () {
                 // Text 2이 눌렸을 때 실행되는 동작
                 ScaffoldMessenger.of(context).showSnackBar(
@@ -35,11 +47,31 @@ class InfoPage extends StatelessWidget {
             ),
             SizedBox(height: 16.0),
             InfoText(
-              text: 'Text 3',
+              text: Text3,
               onTap: () {
                 // Text 3이 눌렸을 때 실행되는 동작
                 ScaffoldMessenger.of(context).showSnackBar(
                   SnackBar(content: Text('Text 3 clicked')),
+                );
+              },
+            ),
+            SizedBox(height: 16.0),
+            InfoText(
+              text: Text4,
+              onTap: () {
+                // Text 3이 눌렸을 때 실행되는 동작
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Text 4 clicked')),
+                );
+              },
+            ),
+            SizedBox(height: 16.0),
+            InfoText(
+              text: Text5,
+              onTap: () {
+                // Text 3이 눌렸을 때 실행되는 동작
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Text 5 clicked')),
                 );
               },
             ),
